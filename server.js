eea89f372f86f946892c00fa7d02a45d94502164
shovvel <shovvel@shovvel.com>
2021-10-25 16:08:06 +0900

    Add Code

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const server = net.createServer(function (client) {
   console.log("result 확인 : " + result);
 
   // client.write(result);
-  client.write('[3G*9024011309*0002*CR]');
+  client.write('[3G*9024025633*0002*CR]');
   console.log("클라이언트 : " + `[3G*` + imei + `*0002*CR]`);
 
 
