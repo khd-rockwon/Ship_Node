fb40af0e517cf69e4b356301a118ff7dec583237
shovvel <shovvel@shovvel.com>
2021-10-25 15:52:50 +0900

    Add Code

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const net = require('net')
+const net = require('net');
 
-const maria = require('mysql')
+const maria = require('mysql');
 const {response} = require("express");
 
 var connection = maria.createConnection({
+
   host:'192.168.50.54',
   post:3306,
   user:'shovvel',
   password:'rockwon12!',
   database:'TEST'
+
 })
 
 
@@ -20,41 +22,14 @@ const server = net.createServer(function (client) {
   console.log('   local = %s:%s', client.localAddress, client.localPort);
   console.log('   remote = %s:%s', client.remoteAddress, client.remotePort);
 
-  // client.write('[3G*9024025633*0002*CR]');
-
-  // client.write('[3G*9024025495*0002*CR]');
-  // client.write('[3G*9024012880*0002*CR]');
-  // client.write('[3G*9024011797*0002*CR]');
-  // client.write('[3G*9024025762*0002*CR]');
-  // client.write('[3G*9024025584*0002*CR]');
-  // client.write('[3G*9024025673*0002*CR]');
-  // client.write('[3G*9024011309*0002*CR]');
-  // client.write('[3G*9024025509*0002*CR]');
-  // client.write('[3G*9024025664*0002*CR]');
-  // client.write('[3G*9024025633*0002*CR]');
-  // client.write('[3G*9024025484*0002*CR]');
-  // client.write('[3G*9024025290*0002*CR]');
-  // client.write('[3G*9024024780*0002*CR]');
-  // client.write('[3G*9024025519*0002*CR]');
-  // client.write('[3G*9024025653*0002*CR]');
-  // client.write('[3G*9024025277*0002*CR]');
-  // client.write('[3G*9024025568*0002*CR]');
-  // client.write('[3G*9024025434*0002*CR]');
-  // client.write('[3G*9024011379*0002*CR]');
-  // client.write('[3G*9024025352*0002*CR]');
-  // client.write('[3G*9024025048*0002*CR]');
-  // client.write('[3G*9024025413*0002*CR]');
-
-
-
   client.on('data', function (data) {
     console.log("확인1 : " + data.toString());
 
     imei = data.slice(4,14);
     var stringData = data.toString();
 
-     console.log("data.slice : " + data.slice(4,14));
-     console.log("imei확인1 : " + imei);
+    console.log("data.slice : " + data.slice(4,14));
+    console.log("imei확인1 : " + imei);
     console.log("stringData : " + stringData);
 
     connection.query('INSERT INTO wearableData(data) VALUES (?)', [stringData],
@@ -78,7 +53,8 @@ const server = net.createServer(function (client) {
   result = '[3G*'.concat(imei, `*0002*CR]`);
   console.log("result 확인 : " + result);
 
-  client.write(result);
+  // client.write(result);
+  client.write('[3G*9024011309*0002*CR]');
   console.log("클라이언트 : " + `[3G*` + imei + `*0002*CR]`);
 
 
@@ -88,10 +64,13 @@ const server = net.createServer(function (client) {
 })
 
 server.listen(30000, function () {
+
   console.log('Server listening: ' + JSON.stringify(server.address()))
+
   server.on('close', function () {
     console.log('Server Terminated')
   })
+
   server.on('error', function (err) {
     console.log('Server Error: ', JSON.stringify(err))
   })
